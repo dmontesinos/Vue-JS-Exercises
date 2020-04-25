e8be8a9e8dea187487e9efa0cb4169440cd0e6c2
dmontesinos <danimontesinos1@gmail.com>
2020-04-26 01:18:37 +0200

    Ejercicio 10 hecho

--- actual.js
+++ actual.js
@@ -1,27 +1,35 @@
-//Ejercicio 9
+//Ejercicio 10
 var vm = new Vue({
   el: '#app',
   data: {
-    estado: 0,
-  },
+    personas: [
+      { name: 'Jaime Sommers', phone: '311-555-2368' },
+      { name: 'Ghostbusters', phone: '55-2368' },
+      { name: 'Mr. Plow', phone: '636-555-3226' },
+      { name: 'Gene Parmesan: Private Eye', phone: '555-0113' },
+      { name: 'The A-Team', phone: '555-6162' },
+    ],
 
-  watch: {
-    estado: function() {
-      if(this.estado == 3){
-        this.estado = 0;
-      }
+  },
+  methods: {
+    eliminarElemento: function(index) {
+      this.personas.splice(index,1);
     }
   },
+
   template: `<div>
-    <div style="display: inline-block; width:30px;">
-    <div v-if="estado == 0 || estado == 1" style="height: 30px; background-color: indianRed"></div>
-    <div v-if="estado == 2" style="height: 30px; background-color: red"></div>
-    <div v-if="estado == 0 || estado == 2" style="height: 30px; background-color: khaki"></div>
-    <div v-if="estado == 1" style="height: 30px; background-color: yellow"></div>
-    <div v-if="estado == 1 || estado == 2" style="height: 30px; background-color: seagreen"></div>
-    <div v-if="estado == 0" style="height: 30px; background-color: green"></div>
-    <button v-on:click="estado++"> Switch! </button>
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>Phone number</th>
+      <th>Delete</th>
+    </tr>
+    <tr v-for="(persona, index) in personas">
+      <td>{{persona.name}}</td>
+      <td>{{persona.phone}}</td>
+      <td><button v-on:click="eliminarElemento(index)">Delete</button></td>
+    </tr>
+  </table>
     </div>
-  </div>
   `,
 })
--- problemas.js
+++ problemas.js
@@ -203,3 +203,39 @@ var vm = new Vue({
   </div>
   `,
 })
+
+//Ejercicio 10
+var vm = new Vue({
+  el: '#app',
+  data: {
+    personas: [
+      { name: 'Jaime Sommers', phone: '311-555-2368' },
+      { name: 'Ghostbusters', phone: '55-2368' },
+      { name: 'Mr. Plow', phone: '636-555-3226' },
+      { name: 'Gene Parmesan: Private Eye', phone: '555-0113' },
+      { name: 'The A-Team', phone: '555-6162' },
+    ],
+
+  },
+  methods: {
+    eliminarElemento: function(index) {
+      this.personas.splice(index,1);
+    }
+  },
+
+  template: `<div>
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>Phone number</th>
+      <th>Delete</th>
+    </tr>
+    <tr v-for="(persona, index) in personas">
+      <td>{{persona.name}}</td>
+      <td>{{persona.phone}}</td>
+      <td><button v-on:click="eliminarElemento(index)">Delete</button></td>
+    </tr>
+  </table>
+    </div>
+  `,
+})
