4ca8891f88e1f14b37426740f3de53181cd8e0bd
dmontesinos <danimontesinos1@gmail.com>
2020-04-26 17:09:09 +0200

    Acabado el ejercicio 11 - Crear componente - Modificar atributos mediante props - splice para separar elementos por delimitador "espacio"

--- actual.js
+++ actual.js
@@ -1,35 +1,28 @@
 //Ejercicio 10
-var vm = new Vue({
-  el: '#app',
-  data: {
-    personas: [
-      { name: 'Jaime Sommers', phone: '311-555-2368' },
-      { name: 'Ghostbusters', phone: '55-2368' },
-      { name: 'Mr. Plow', phone: '636-555-3226' },
-      { name: 'Gene Parmesan: Private Eye', phone: '555-0113' },
-      { name: 'The A-Team', phone: '555-6162' },
-    ],
-
-  },
-  methods: {
-    eliminarElemento: function(index) {
-      this.personas.splice(index,1);
+Vue.component('words-to-list', {
+  data: function(){
+    return {
+      palabrasSeparadas: "",
     }
   },
+  props: ['words'], //Coge el tag/atributo del html
+  template: `
+  <div>
+    <ul v-for="palabra in palabrasSeparadas">
+      <li>{{palabra}}</li>
+    </ul>
+  </div>
+  `,
+  created: function() { //Se ejecuta al crear una nueva instancia del componente.
+    //Prueba que demuestra que es un vinculo reactivo (se modifica el tag desde
+    // fuera del propio html).  //this.words = this.words += ' hola';
+    this.palabrasSeparadas = this.words.split(' ');
+  },
+})
 
-  template: `<div>
-  <table>
-    <tr>
-      <th>Name</th>
-      <th>Phone number</th>
-      <th>Delete</th>
-    </tr>
-    <tr v-for="(persona, index) in personas">
-      <td>{{persona.name}}</td>
-      <td>{{persona.phone}}</td>
-      <td><button v-on:click="eliminarElemento(index)">Delete</button></td>
-    </tr>
-  </table>
-    </div>
+var vm = new Vue({
+  el: "#app",
+  template: `
+    <words-to-list words="Lorem ipsum dolor sit amet"></words-to-list>
   `,
 })
